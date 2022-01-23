886d92c7f8ae406f8a43e8798c08194845861fd4
Umar Qayyum <slipa_jepun96@outlook.com>
2022-01-23 22:04:57 +0800

    new commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.master')
+
+@section('content')
+<div class="m-3 bg-white w-auto rounded-xl p-3">
+    <p class="text-2xl font-bold">FFB Output Of All Estates (MT)</p>
+    <p class="italic text-gray-700">Last Updates: 18 January 2022</p>
+    <p>~Graph here~</p>
+</div>
+@endsection
