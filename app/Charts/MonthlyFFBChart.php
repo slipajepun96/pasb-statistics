ecaa4bd2252bf42dd9abb864e32c1ae450049c46
Umar Qayyum <slipa_jepun96@outlook.com>
2023-11-05 12:51:06 +0800

    changes on index's graph, using actual data

--- app/Charts/MonthlyFFBChart.php
+++ app/Charts/MonthlyFFBChart.php
@@ -13,14 +13,23 @@ class MonthlyFFBChart
         $this->chart = $chart;
     }
 
-    public function build($data_array): \ArielMejiaDev\LarapexCharts\LineChart
+    public function build($graph_data): \ArielMejiaDev\LarapexCharts\LineChart
     {
-        // dd($data_array);
+        // dd($graph_data);
+
+        //ffb yield to view
+        $yesterday_month=$graph_data[2];
+        for($i=0;$i<$yesterday_month;$i++)
+        {
+            $ffb_yield[$i]=$graph_data[0][$i];
+        }
+
         return $this->chart->lineChart()
             ->setTitle('Actual FFB Yield (MT)')
             // ->setSubtitle('Physical sales vs Digital sales.')
-            ->addData('Actual', [40, 93, 35, 42, 18, 82, 100, 90, 70, 90])
-            ->addData('Budget', [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200])
+            // ->addData('Actual', [40, 93, 35, 42, 18, 82, 100, 90, 70, 90])
+            ->addData('Actual', $ffb_yield)
+            ->addData('Budget', $graph_data[1])
             ->setXAxis(['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'])
             ->setGrid('#3F51B5', 0.1);
     }
--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -19,21 +19,21 @@ class IndexController extends Controller
         $current_year=date("Y");
         // $current_year=2022;
         $yesterday_date=date('d.m.Y',strtotime("-1 days"));
-        // dd($yesterday_year);
+        
         // $yesterday_month=12;
         // $yesterday_year=2022;
 
         $yesterday_year=date('Y', strtotime($yesterday_date));
         $yesterday_month=date('m', strtotime($yesterday_date));
         $last_year=$current_year-1;
-
+        // dd($yesterday_date);
 
         $estates_area=AreaEstate::select(['matured_area','estate_id'])->where('current_year','=',$current_year)->get();
         $estates=Estate::select(['estate_name','id','abbreviation'])->orderBy('id','ASC')->get();
         $ffbyields=DailyYield::select(['id','date','estate_id','ffb_mt'])->where([['year','=',$yesterday_year],['month','=',$yesterday_month]])->orderBy('date','ASC')->orderBy('estate_id','ASC')->get();
         $cumulative_ffb_mts=CumulativeFfb::select(['year','month','estate_id','cumulative_ffb_mt','latest_ffb_date'])->where('year','=',$yesterday_year)->get();
         $cumulative_ffb_mts_last_year=CumulativeFfb::select(['year','month','estate_id','cumulative_ffb_mt','latest_ffb_date'])->where('year','=',$last_year)->get();
-        // dd($cumulative_ffb_mts_last_year);
+        // dd($cumulative_ffb_mts);
 
         //calculate current year ffb output
         $total_ffbmt_yearly=0;
@@ -48,7 +48,6 @@ class IndexController extends Controller
         {
             $last_year_total_ffb_mt_yearly=$last_year_total_ffb_mt_yearly+$cumulative_ffb_mt_last_year->cumulative_ffb_mt;
         }
-
 
         //calculate YPH
         $total_matured_area=0;
@@ -89,7 +88,12 @@ class IndexController extends Controller
             $estate_yph[$i][2]=$estate_yph[$i][2]+$estate_yph[$i][3];
             $i=$i+1;
         }
+
+        //passing monthly budget and monthly ffb to chart
+        $monthly_ffb_budget[]=0;
+
         $company_yph=$total_ffbmt_yearly/$total_matured_area;
+
 
         //calculate estate YPH
         foreach($cumulative_ffb_mts as $cumulative_ffb_mt)
@@ -105,10 +109,113 @@ class IndexController extends Controller
         $data_array[5]=$estates->values();
 
         //data for graph
-        // $graph_data[0][0];//budget
-        
+        $graph_data=$this->getGraphData($yesterday_year,$yesterday_month);
 
         // dd($estate_yph[1][3]);
-        return view('index',['data_array'=>$data_array,'chart'=>$chart->build($data_array)]);
+        return view('index',['data_array'=>$data_array,'chart'=>$chart->build($graph_data)]);
+    }
+
+    public function getGraphData($year,$yesterday_month)
+    {
+        //calculate monthly ffb yield 
+        for($i=0;$i<12;$i++)
+        {
+            if($i<10)
+                 $month="0".$i+1;
+            else
+                $month=$i+1;
+
+            $graph_data[0][$i]=$this->getMonthlyFFBYieldForCompany($month,$year);
+        }
+
+        //calculate budget
+        for($j=0;$j<12;$j++)
+        {
+            if($j<10)
+                 $month="0".$j+1;
+            else
+                $month=$j+1;
+            $graph_data[1][$j]=$this->getMonthlyFFBBudgetForCompany($month,2023);
+        }
+        
+        $graph_data[2]=$yesterday_month;
+        return $graph_data;
+    }
+
+    public function getMonthlyFFBYieldForCompany($month,$year)
+    {
+        $sum_cum_ffb=0;
+        $cum_ffbs=CumulativeFfb::select(['year','month','estate_id','cumulative_ffb_mt','latest_ffb_date'])->where('year','=',$year)->where('month','=',$month)->get();
+        //calculate cumulative ffb for that month
+        foreach($cum_ffbs as $cum_ffb)
+        {
+            
+            $sum_cum_ffb=$sum_cum_ffb + $cum_ffb->cumulative_ffb_mt;
+        }
+        if($sum_cum_ffb==0)
+            $sum_cum_ffb=null;
+        
+        return round($sum_cum_ffb,2);
+    }
+
+    public function getMonthlyFFBBudgetForCompany($month,$year)
+    {
+        $sum_budget_ffb=0;
+        $cum_budgets=Budget::select(['jan_budget_mt','feb_budget_mt','mac_budget_mt','apr_budget_mt','may_budget_mt','june_budget_mt','july_budget_mt','aug_budget_mt','sept_budget_mt','oct_budget_mt','nov_budget_mt','dec_budget_mt',])->where('year','=',$year)->get();
+
+        foreach($cum_budgets as $cum_budget)
+        {
+            if($month=="01")
+            {
+                $sum_budget_ffb=$sum_budget_ffb+$cum_budget->jan_budget_mt;
+            }
+            else if($month=="02")
+            {
+                $sum_budget_ffb=$sum_budget_ffb+$cum_budget->feb_budget_mt;
+            }
+            else if($month=="03")
+            {
+                $sum_budget_ffb=$sum_budget_ffb+$cum_budget->mac_budget_mt;
+            }
+            else if($month=="04")
+            {
+                $sum_budget_ffb=$sum_budget_ffb+$cum_budget->apr_budget_mt;
+            }
+            else if($month=="05")
+            {
+                $sum_budget_ffb=$sum_budget_ffb+$cum_budget->may_budget_mt;
+            }
+            else if($month=="06")
+            {
+                $sum_budget_ffb=$sum_budget_ffb+$cum_budget->june_budget_mt;
+            }
+            else if($month=="07")
+            {
+                $sum_budget_ffb=$sum_budget_ffb+$cum_budget->july_budget_mt;
+            }
+            else if($month=="08")
+            {
+                $sum_budget_ffb=$sum_budget_ffb+$cum_budget->aug_budget_mt;
+            }
+            else if($month=="09")
+            {
+                $sum_budget_ffb=$sum_budget_ffb+$cum_budget->sept_budget_mt;
+            }
+            else if($month=="10")
+            {
+                $sum_budget_ffb=$sum_budget_ffb+$cum_budget->oct_budget_mt;
+            }
+            else if($month=="11")
+            {
+                $sum_budget_ffb=$sum_budget_ffb+$cum_budget->nov_budget_mt;
+            }
+            else if($month=="12")
+            {
+                $sum_budget_ffb=$sum_budget_ffb+$cum_budget->dec_budget_mt;
+            }
+            else
+                $sum_budget_ffb="Error";
+        }
+        return round($sum_budget_ffb,2);
     }
 }
